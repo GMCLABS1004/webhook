fb0fa0e64edd4b0e21a61109d6ba3b48c94c3557
jtk4556 <jtk4556@naver.com>
2019-09-02 17:45:38 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/api/webhook',function(req,res, next){
+  console.log("req.body : " + req.body);
+  res.send({});
+});
+
+
+module.exports = router;
