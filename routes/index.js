ad0d69df049d8d926298576db8ada1f5c46c3d96
jtk4556 <jtk4556@naver.com>
2019-09-02 21:54:35 +0900

    ㅏㅏ

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/api/webhook',function(req,res, next){
-  console.log("[" + new Date().toISOString() + "] : " + JSON.stringify(req));
-  //res.send({});
+  console.log("[" + new Date() + "] : " + JSON.stringify(req));
+  res.send({});
 });
 
 
