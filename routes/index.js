d73d688eaa14e9c80df3ace74894058e39878a9c
jtk4556 <jtk4556@naver.com>
2019-09-03 11:58:26 +0900

    ㄷㄷ

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var date = new Date();
   console.log("[" + date.toISOString() + "] : " + req.body);
   res.render('index', { title: 'Express' });
 });
