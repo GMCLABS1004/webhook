3fa009b92d79013d3f514f10dd2bb5ca8600e407
jtk4556 <jtk4556@naver.com>
2019-09-06 20:31:31 +0900

    aefaef

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var webSetting = require('./webSetting.json');
 var app = express();
 
 // view engine setup
@@ -23,7 +23,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next){
   next(createError(404));
 });
 
@@ -37,5 +37,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+mongoose.connect(webSetting.dbPath);
+
 
 module.exports = app;
